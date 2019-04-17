ec3fdd6b5187d46cb9747ef095139ae1d8950b2b
teaetik <victorKhovTko@yandex.ru>
2019-04-17 15:31:58 -0700

    Added Task-3.2

---
+++ Source/ProblemSet03/Task-3.2/Task-3.2/HashFromStringFunction.cs
@@ -0,0 +1,10 @@
+﻿namespace ProblemSet03.Task02
+{
+    public class HashFromStringFunction : IHashFunction<string>
+    {
+        public int GetHash(string value)
+        {
+            return value?.GetHashCode() + 13 ?? 0;
+        }
+    }
+}
