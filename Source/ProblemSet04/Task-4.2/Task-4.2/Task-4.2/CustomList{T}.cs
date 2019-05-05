402dc7f6fde781a70318b066ca5a7be1bc314b1a
teaetik <victorKhovTko@yandex.ru>
2019-05-05 14:08:13 -0700

    Added solution for Task-4.2

---
+++ Source/ProblemSet04/Task-4.2/Task-4.2/Task-4.2/CustomList{T}.cs
@@ -0,0 +1,230 @@
+﻿namespace ProblemSet04.Task02
+{
+    using System;
+
+    /// <summary>
+    /// Represents a strongly typed list of objects that can be accessed by index.
+    /// Provides methods to manipulate lists.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the list</typeparam>
+    public class CustomList<T>
+    {
+        /// <summary>
+        /// Gets pointer to the first element
+        /// </summary>
+        public CustomListElement<T> Head { get; private set; }
+
+        /// <summary>
+        /// Gets number of elements of the list
+        /// </summary>
+        public int Length { get; private set; }
+
+        /// <summary>
+        /// Gets a value indicating whether the IList is read-only
+        /// </summary>
+        public bool IsReadOnly => false;
+
+        public T this[int index] => GetValueByPosition(index);
+
+        /// <summary>
+        /// Adds new element on specified position.
+        /// Indexes starts from zero
+        /// </summary>
+        /// <param name="value">new value to add</param>
+        /// <param name="position">index in the list</param>
+        public void AddValueOnPosition(T value, int position)
+        {
+            if (position < 0 || position > Length)
+            {
+                throw new IndexOutOfRangeException($"Position {position} is incorrect");
+            }
+
+            if (position == 0)
+            {
+                var newHeadElement = new CustomListElement<T>(value, Head);
+                Head = newHeadElement;
+                ++Length;
+                return;
+            }
+
+            var cursor = GetElementByPosition(position - 1);
+            var newElement = new CustomListElement<T>(value, cursor.Next);
+            cursor.Next = newElement;
+            ++Length;
+        }
+
+        /// <summary>
+        /// Gets value by position
+        /// </summary>
+        /// <param name="position">index of element in the list</param>
+        /// <returns>value of element on specified position</returns>
+        public T GetValueByPosition(int position)
+        {
+            if (position < 0 || position >= Length)
+            {
+                throw new IndexOutOfRangeException($"Position {position} is incorrect");
+            }
+
+            var cursor = GetElementByPosition(position);
+            return cursor.Value;
+        }
+
+        /// <summary>
+        /// Gets element by position
+        /// </summary>
+        /// <param name="position">position of the element</param>
+        /// <returns>element on specified position</returns>
+        public CustomListElement<T> GetElementByPosition(int position)
+        {
+            var cursor = Head;
+            for (var i = 0; i < position; ++i)
+            {
+                cursor = cursor.Next;
+            }
+
+            return cursor;
+        }
+
+        /// <summary>
+        /// Deletes element on specified position
+        /// </summary>
+        /// <param name="position">index of element to delete</param>
+        public virtual void RemoveElementByPosition(int position)
+        {
+            if (Length == 0)
+            {
+                throw new EmptyListException("Could not delete from empty list");
+            }
+
+            if (position < 0 || position >= Length)
+            {
+                throw new IndexOutOfRangeException($"Position {position} is incorrect");
+            }
+
+            if (position == 0)
+            {
+                Head = Head.Next;
+                --Length;
+                return;
+            }
+
+            var cursor = GetElementByPosition(position - 1);
+            cursor.Next = cursor.Next.Next;
+            --Length;
+        }
+
+        /// <summary>
+        /// Removes the first occurrence of a specific object from the list.
+        /// </summary>
+        /// <param name="value">value to remove</param>
+        /// <returns>true if item is successfully removed; otherwise, false</returns>
+        public virtual bool Remove(T value)
+        {
+            if (Length == 0)
+            {
+                throw new EmptyListException("Could not delete from empty list");
+            }
+
+            if (ElementEquals(Head.Value, value))
+            {
+                Head = Head.Next;
+                --Length;
+                return true;
+            }
+
+            var current = Head;
+            for (var i = 0; i < Length - 1; ++i)
+            {
+                if (ElementEquals(current.Next.Value, value))
+                {
+                    current.Next = current.Next.Next;
+                    --Length;
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if list is empty
+        /// </summary>
+        /// <returns>true if list is empty, otherwise false</returns>
+        public bool IsEmpty() => Length == 0;
+
+        /// <summary>
+        /// Adds new specific value to the list
+        /// </summary>
+        /// <param name="value">value to add</param>
+        public virtual void AddNew(T value)
+        {
+            AddValueOnPosition(value, Length);
+        }
+
+        /// <summary>
+        /// Removes all elements from the list
+        /// </summary>
+        public void Clear()
+        {
+            Head = null;
+            Length = 0;
+        }
+
+        /// <summary>
+        /// Copies the entire list to a compatible one-dimensional array,
+        /// starting at the specified index of the target array
+        /// </summary>
+        /// <param name="array">The one-dimensional Array that is the destination of the elements copied from list</param>
+        /// <param name="arrayIndex">The zero-based index in array at which copying begins</param>
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (arrayIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (array.Length - arrayIndex < Length)
+            {
+                throw new ArgumentException();
+            }
+
+            var counter = 0;
+            for (var current = Head; current != null; current = current.Next)
+            {
+                array[arrayIndex + counter] = current.Value;
+                counter++;
+            }
+        }
+
+        /// <summary>
+        /// Determines whether an element is in the list
+        /// </summary>
+        /// <param name="value">The object to locate in the list</param>
+        /// <returns>true if item is found in the list; otherwise, false</returns>
+        public bool Contains(T value)
+        {
+            var current = Head;
+
+            for (var i = 0; i < Length; ++i)
+            {
+                if (ElementEquals(current.Value, value))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        private static bool ElementEquals(T left, T right) => object.Equals(left, right);
+    }
+}
---
+++ Source/ProblemSet04/Task-4.2/Task-4.2/Task-4.2/UniqueList.cs
@@ -0,0 +1,39 @@
+﻿namespace ProblemSet04.Task02
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Class implements linked list where every value is unique.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the list</typeparam>
+    public class UniqueList<T> : CustomList<T>
+    {
+        /// <summary>
+        /// Adds new specific unique value to the list
+        /// </summary>
+        /// <param name="value">unique value to add</param>
+        public override void AddNew(T value)
+        {
+            if (Contains(value))
+            {
+                throw new AddContainedValueException($"The value:{value} is already in the list");
+            }
+
+            base.AddNew(value);
+        }
+
+        /// <summary>
+        /// Removes occurrence of a specific unique value from the list.
+        /// </summary>
+        /// <param name="value">value to remove</param>
+        /// <returns>true if item is successfully removed; otherwise, false</returns>
+        public override bool Remove(T value)
+        {
+            if (!Contains(value))
+            {
+                throw new RemoveNotContainedValueException($"The value:{value} is not contained in the list");
+            }
+
+            return base.Remove(value);
+        }
+    }
+}
